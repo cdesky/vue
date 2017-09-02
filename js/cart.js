3e9c494eb60d3e73f541631f4a0b5ae22d68cdad
cdesky <1724024479@qq.com>
2017-09-02 21:58:13 +0800

    shop

--- js/cart.js
+++ js/cart.js
@@ -2,22 +2,59 @@ new Vue({
   el: '#app',
   data: {
   	total:0,
-   productList:[]
+   productList:[],
+   checkAllFlag:false
   },
   filters: {
-   
+   formaterMoney:function(val){
+   	return '￥'+val.toFixed(2)
+   }
   },
   mounted: function() {
      this.loadData()
   },
   methods: {
+  		//初始化数据
       loadData:function(){
         var _this=this;
         this.$http.get('data/cart.json?t='+new Date().getTime()).then(function(res){
             _this.productList=res.body.result.list
             _this.total=res.body.result.totalMoney
         })
+      },
+      //数量的加减
+      addCount:function(item,way){
+      	if(way>0)
+      		item.count++;
+      	else
+      	{
+      		item.count--;
+      		if(item.count<1)
+      			item.count=1;
+      	}
+      },
+      //选中 取消 某条数据
+      select:function(item){
+      	if(typeof item.checked=='undefined') 
+      		Vue.set(item,'checked',true)
+      	else
+      		item.checked=!item.checked	
+      },
+      //全选  取消
+      checkAll:function(flag){
+      	this.checkAllFlag=flag
+      	var _this=this;
+      	this.productList.forEach(function(val,index){
+      		if(typeof val.checked=='undefined')
+	      		Vue.set(val,'checked',_this.checkAllFlag)
+	      	else
+	      		val.checked=_this.checkAllFlag
+      	})
       }
 
     }
 });
+
+Vue.filter('money',function(val,type){
+	return '$'+val.toFixed(2)+type
+})
